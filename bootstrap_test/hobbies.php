724ad5b0f2ce00ba2bc053d5ac2308cc17dfc7e7
youyoulebgdemeknes <winghouserv511@gmail.com>
2023-03-20 00:40:56 +0100

    bootstrap v6

--- bootstrap_test/hobbies.php
+++ bootstrap_test/hobbies.php
@@ -1,50 +1,63 @@
 <?php
-    require_once('template_header.php');
-    require_once('template_menu.php');
-    renderMenuToHTML("hobbies") ;
+require_once('template_header.php');
+require_once('template_menu.php');
+renderMenuToHTML("info_tech") ;
 ?>
-<hr class="m-0" />
-            <!-- hobbies-->
+            <!-- Interests-->
             <section class="resume-section" id="hobbies">
                 <div class="resume-section-content">
-                    <h2 class="mb-5">hobbies</h2>
-                    <div class="subheading mb-3">Langage de Programation & Outils </div>
-                    <ul class="list-inline dev-icons">
-                        <li class="list-inline-item"><i class="fab fa-html5"></i></li>
-                        <li class="list-inline-item"><i class="fab fa-css3-alt"></i></li>
-                        <li class="list-inline-item"><i class="fab fa-js-square"></i></li>
-                        <li class="list-inline-item"><i class="fab fa-angular"></i></li>
-                        <li class="list-inline-item"><i class="fab fa-react"></i></li>
-                        <li class="list-inline-item"><i class="fab fa-node-js"></i></li>
-                        <li class="list-inline-item"><i class="fab fa-sass"></i></li>
-                        <li class="list-inline-item"><i class="fab fa-less"></i></li>
-                        <li class="list-inline-item"><i class="fab fa-wordpress"></i></li>
-                        <li class="list-inline-item"><i class="fab fa-gulp"></i></li>
-                        <li class="list-inline-item"><i class="fab fa-grunt"></i></li>
-                        <li class="list-inline-item"><i class="fab fa-npm"></i></li>
-                    </ul>
-                    <div class="subheading mb-3">Workflow</div>
+                    <h2 class="mb-5">Mes Hobbies</h2>
+                    <p>En plus d'être un développeur Web, j'apprécie la plupart de mon temps à l'extérieur. En hiver, je suis un skieur passionné et un grimpeur de glace novice. Pendant les mois les plus chauds ici à Lille, j'aime faire du VTT, de l'escalade libre et du kayak.</p>
+                    <p class="mb-0">Lorsque je suis forcé à l'intérieur, je suis un certain nombre de films et d'émissions de télévision de genre science-fiction et fantastique, je suis un chef en herbe et je passe une grande partie de mon temps libre à explorer les dernières avancées technologiques dans le monde du développement Web front end.</p>
+                </div>
+            </section>
+            <hr class="m-0" />
+            <!-- Awards-->
+            <section class="resume-section" id="awards">
+                <div class="resume-section-content">
+                    <h2 class="mb-5">Awards & Certifications</h2>
                     <ul class="fa-ul mb-0">
                         <li>
-                            <span class="fa-li"><i class="fas fa-check"></i></span>
-                            Mobile-First, Responsive Design
+                            <span class="fa-li"><i class="fas fa-trophy text-warning"></i></span>
+                            Google Analytics Certified Developer
                         </li>
                         <li>
-                            <span class="fa-li"><i class="fas fa-check"></i></span>
-                            Cross Browser Testing & Debugging
+                            <span class="fa-li"><i class="fas fa-trophy text-warning"></i></span>
+                            Mobile Web Specialist - Google Certification
                         </li>
                         <li>
-                            <span class="fa-li"><i class="fas fa-check"></i></span>
-                            Cross Functional Teams
+                            <span class="fa-li"><i class="fas fa-trophy text-warning"></i></span>
+                            1
+                            <sup>st</sup>
+                            Place - University of Colorado Boulder - Emerging Tech Competition 2009
                         </li>
                         <li>
-                            <span class="fa-li"><i class="fas fa-check"></i></span>
-                            Agile Development & Scrum
+                            <span class="fa-li"><i class="fas fa-trophy text-warning"></i></span>
+                            1
+                            <sup>st</sup>
+                            Place - University of Colorado Boulder - Adobe Creative Jam 2008 (UI Design Category)
+                        </li>
+                        <li>
+                            <span class="fa-li"><i class="fas fa-trophy text-warning"></i></span>
+                            2
+                            <sup>nd</sup>
+                            Place - University of Colorado Boulder - Emerging Tech Competition 2008
+                        </li>
+                        <li>
+                            <span class="fa-li"><i class="fas fa-trophy text-warning"></i></span>
+                            1
+                            <sup>st</sup>
+                            Place - James Buchanan High School - Hackathon 2006
+                        </li>
+                        <li>
+                            <span class="fa-li"><i class="fas fa-trophy text-warning"></i></span>
+                            3
+                            <sup>rd</sup>
+                            Place - James Buchanan High School - Hackathon 2005
                         </li>
                     </ul>
                 </div>
             </section>
-    <hr class="m-0" />
 <?php
     require_once('template_footers.php');
 ?>   
--- bootstrap_test/info_tech.php
+++ bootstrap_test/info_tech.php
@@ -1,63 +1,50 @@
 <?php
-require_once('template_header.php');
-require_once('template_menu.php');
-renderMenuToHTML("info_tech") ;
+    require_once('template_header.php');
+    require_once('template_menu.php');
+    renderMenuToHTML("hobbies") ;
 ?>
-            <!-- Interests-->
-            <section class="resume-section" id="interests">
+<hr class="m-0" />
+            <!-- hobbies-->
+            <section class="resume-section" id="info_tech">
                 <div class="resume-section-content">
-                    <h2 class="mb-5">Interests</h2>
-                    <p>Apart from being a web developer, I enjoy most of my time being outdoors. In the winter, I am an avid skier and novice ice climber. During the warmer months here in Colorado, I enjoy mountain biking, free climbing, and kayaking.</p>
-                    <p class="mb-0">When forced indoors, I follow a number of sci-fi and fantasy genre movies and television shows, I am an aspiring chef, and I spend a large amount of my free time exploring the latest technology advancements in the front-end web development world.</p>
-                </div>
-            </section>
-            <hr class="m-0" />
-            <!-- Awards-->
-            <section class="resume-section" id="awards">
-                <div class="resume-section-content">
-                    <h2 class="mb-5">Awards & Certifications</h2>
+                    <h2 class="mb-5">Information Technique</h2>
+                    <div class="subheading mb-3">Langage de Programation & Outils </div>
+                    <ul class="list-inline dev-icons">
+                        <li class="list-inline-item"><i class="fab fa-html5"></i></li>
+                        <li class="list-inline-item"><i class="fab fa-css3-alt"></i></li>
+                        <li class="list-inline-item"><i class="fab fa-js-square"></i></li>
+                        <li class="list-inline-item"><i class="fab fa-angular"></i></li>
+                        <li class="list-inline-item"><i class="fab fa-react"></i></li>
+                        <li class="list-inline-item"><i class="fab fa-node-js"></i></li>
+                        <li class="list-inline-item"><i class="fab fa-sass"></i></li>
+                        <li class="list-inline-item"><i class="fab fa-less"></i></li>
+                        <li class="list-inline-item"><i class="fab fa-wordpress"></i></li>
+                        <li class="list-inline-item"><i class="fab fa-gulp"></i></li>
+                        <li class="list-inline-item"><i class="fab fa-grunt"></i></li>
+                        <li class="list-inline-item"><i class="fab fa-npm"></i></li>
+                    </ul>
+                    <div class="subheading mb-3">Mes Cappacité</div>
                     <ul class="fa-ul mb-0">
                         <li>
-                            <span class="fa-li"><i class="fas fa-trophy text-warning"></i></span>
-                            Google Analytics Certified Developer
+                            <span class="fa-li"><i class="fas fa-check"></i></span>
+                            Design d'apllication
                         </li>
                         <li>
-                            <span class="fa-li"><i class="fas fa-trophy text-warning"></i></span>
-                            Mobile Web Specialist - Google Certification
+                            <span class="fa-li"><i class="fas fa-check"></i></span>
+                            Développement front end
                         </li>
                         <li>
-                            <span class="fa-li"><i class="fas fa-trophy text-warning"></i></span>
-                            1
-                            <sup>st</sup>
-                            Place - University of Colorado Boulder - Emerging Tech Competition 2009
+                            <span class="fa-li"><i class="fas fa-check"></i></span>
+                            Travail en equipe
                         </li>
                         <li>
-                            <span class="fa-li"><i class="fas fa-trophy text-warning"></i></span>
-                            1
-                            <sup>st</sup>
-                            Place - University of Colorado Boulder - Adobe Creative Jam 2008 (UI Design Category)
-                        </li>
-                        <li>
-                            <span class="fa-li"><i class="fas fa-trophy text-warning"></i></span>
-                            2
-                            <sup>nd</sup>
-                            Place - University of Colorado Boulder - Emerging Tech Competition 2008
-                        </li>
-                        <li>
-                            <span class="fa-li"><i class="fas fa-trophy text-warning"></i></span>
-                            1
-                            <sup>st</sup>
-                            Place - James Buchanan High School - Hackathon 2006
-                        </li>
-                        <li>
-                            <span class="fa-li"><i class="fas fa-trophy text-warning"></i></span>
-                            3
-                            <sup>rd</sup>
-                            Place - James Buchanan High School - Hackathon 2005
+                            <span class="fa-li"><i class="fas fa-check"></i></span>
+                            Méthodoogie agile & Scrum
                         </li>
                     </ul>
                 </div>
             </section>
+    <hr class="m-0" />
 <?php
     require_once('template_footers.php');
 ?>   
