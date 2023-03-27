decd01ea7073f4fd1e36bdb2cb4be2d34cf24bed
youyoulebgdemeknes <winghouserv511@gmail.com>
2023-03-27 09:32:32 +0200

    API2

--- TP4/exos5/config.php
+++ TP4/exos5/config.php
@@ -5,11 +5,3 @@ define('_MYSQL_DBNAME','dbtest');
 define('_MYSQL_USER','root');
 define('_MYSQL_PASSWORD','');
  
-
-    $connectionString = "mysql:host=". _MYSQL_HOST;
-
-    if(defined('_MYSQL_PORT'))
-        $connectionString .= ";port=". _MYSQL_PORT;
-
-    $connectionString .= ";dbname=" . _MYSQL_DBNAME;
-    $options = array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8' );
